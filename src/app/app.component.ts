e22b4f805f40891ec0ac65c1c7427dc4b53ed132
rj <rflores@taurix.com>
2024-03-20 22:03:46 -0700

    chore: conditionally render nav off home + add img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RickAndMorty';
+  url = ''
+  constructor(private router: Router){
+    this.url = this.router.url;
+    this.router.events.subscribe(
+      (event: any) => {
+        if (event instanceof NavigationEnd) {
+          this.url = this.router.url;
+        }
+      }
+    );
+  }
 }
