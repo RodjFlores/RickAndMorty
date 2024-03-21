b2d2a0fc98268c279b107c15a1e16131d1a53d65
rj <rflores@taurix.com>
2024-03-20 23:33:26 -0700

    feat: complete landing home page with links and imgs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '../main/home/home.component';
 import { ConnectComponent } from '../main/connect/connect.component';
+import { HomeComponent } from '../main/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
--- src/main/home/home.component.ts
+++ src/main/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from '../../app/app-routing.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
 
+  constructor(    public router: Router, 
+    ){
+      
+    }
+
+  /**
+   * Navigates to a specific route.
+   * @param path Route to be directed to.
+   */
+  public navigateTo(path:string){
+    this.router.navigate([path])
+  }
 }
